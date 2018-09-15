6b75789d8a17a5a335107eedbba037faf24a35ec
David <khudaverdyan>
2018-09-15 20:04:19 +0200

    Initial Commit

---
+++ shaders/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  shaders
+//
+//  Created by David Khudaverdyan on 15/09/2018.
+//  Copyright © 2018 David Khudaverdyan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
