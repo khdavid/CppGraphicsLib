61b357be3ce78f4a153308055946c65ee9470461
David <khudaverdyan>
2018-09-17 20:52:47 +0200

    first commit

--- shaders/main.cpp
+++ shaders/main.cpp
@@ -1,15 +1,37 @@
-//
-//  main.cpp
-//  shaders
-//
-//  Created by David Khudaverdyan on 15/09/2018.
-//  Copyright © 2018 David Khudaverdyan. All rights reserved.
-//
-
+#include "sdl2/sdl.h"
 #include <iostream>
+#include <OpenGl/gl.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int main()
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_CreateWindow(
+                     "",             // window title
+                     SDL_WINDOWPOS_CENTERED,     // x position, centered
+                     SDL_WINDOWPOS_CENTERED,     // y position, centered
+                     640,                        // width, in pixels
+                     480,                        // height, in pixels
+                     SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE           // flags
+                     );
+    
+    std::cout << "Windows in initialized...";
+    SDL_Event e;
+    bool quit = false;
+    while (!quit){
+        while (SDL_PollEvent(&e)){
+            if (e.type == SDL_QUIT){
+                quit = true;
+            }
+            if (e.type == SDL_KEYDOWN){
+                quit = true;
+            }
+            if (e.type == SDL_MOUSEBUTTONDOWN){
+                quit = true;
+            }
+        }
+    }
+
+    
     return 0;
 }
