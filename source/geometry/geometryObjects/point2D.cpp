a361f3a27a32675df5c5993479f9f2e9683c7a94
david <d.khudaverdyan@plaxis.com>
2018-11-08 17:26:15 +0100

    add Point2D class

--- source/geometry/geometryObjects/point2D.cpp
+++ source/geometry/geometryObjects/point2D.cpp
@@ -1,11 +1,46 @@
 #include "point2D.h"
 
 
-Point2D::Point2D()
+Point2D::Point2D(double x, double y) :
+arr_({x, y})
 {
 }
 
-
-Point2D::~Point2D()
+Point2D::Point2D() :
+arr_({0, 0})
 {
 }
+
+double& Point2D::x()
+{
+  return arr_[0];
+}
+
+const double& Point2D::x() const
+{
+  return arr_[0];
+}
+
+double& Point2D::y()
+{
+  return arr_[1];
+}
+
+const double& Point2D::y() const
+{
+  return arr_[1];
+}
+
+double& Point2D::operator[](size_t index)
+{
+  return arr_[index];
+}
+
+const double& Point2D::operator[](size_t index) const
+{
+  return arr_[index];
+}
+
+
+
+
--- source/geometry/geometryObjects/point2D.h
+++ source/geometry/geometryObjects/point2D.h
@@ -1,8 +1,18 @@
 #pragma once
+#include <array>
+
 class Point2D
 {
 public:
   Point2D();
-  ~Point2D();
+  Point2D(double x, double y);
+  double& x();
+  const double& x() const;
+  double& y();
+  const double& y() const;
+  double& operator[](size_t index);
+  const double& operator[](size_t index) const;
+private:
+  std::array<double, 2> arr_;
 };
 
