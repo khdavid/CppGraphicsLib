ed22df6b6e3914b3cc4aa3fbe8343358ca02d32a
david <d.khudaverdyan@plaxis.com>
2018-11-09 16:57:22 +0100

    fix compilation x64

--- source/graphics/main.cpp
+++ source/graphics/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char** argv)
   
   Point2D aa(1, 2);
   auto bb = aa;
-  //bb[0] = 10;
+  bb[0] = 10;
   ContextProvider contextProvider;
   MainLoop(contextProvider).run();
   return 0;
