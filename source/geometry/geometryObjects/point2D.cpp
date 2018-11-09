8cfd8a3b1d9cd7b1a3fbce986dd0c011e53022ed
david <d.khudaverdyan@plaxis.com>
2018-11-09 16:53:02 +0100

    add Point with test in main

--- source/geometry/geometryObjects/point2D.cpp
+++ source/geometry/geometryObjects/point2D.cpp
@@ -1,4 +1,11 @@
 #include "point2D.h"
 
 
+Point2D::Point2D(double x, double y):
+Coord2D(x, y)
+{
+
+}
+
+
 
--- source/graphics/main.cpp
+++ source/graphics/main.cpp
@@ -2,10 +2,15 @@
 
 #include "contextProvider.h"
 #include "eventHandlers/mainLoop.h"
+#include "geometryObjects/point2D.h"
 
 
 int main(int argc, char** argv)
 {
+  
+  Point2D aa(1, 2);
+  auto bb = aa;
+  //bb[0] = 10;
   ContextProvider contextProvider;
   MainLoop(contextProvider).run();
   return 0;
