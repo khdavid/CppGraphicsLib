25c7a90da4a5a3da6a4f4ed4355c3c729224ee98
David <khdavid@mail.ru>
2018-11-10 15:30:26 +0100

    fix visualizers

--- source/graphics/main.cpp
+++ source/graphics/main.cpp
@@ -7,12 +7,15 @@
 
 void test()
 {
+  std::array<int, 2> rr = { 2.3 };
+  rr.size();
   const Vector2D aa(1, 2);
   auto bb = aa * aa*aa / 7;
   bb[0] = 10;
   auto cc = 2 * bb * 2;
   auto tt = Point2D(1, 2);
   auto ttt = Triangle2D(tt, tt, tt);
+  int x = 5;
 }
 
 int main(int argc, char** argv)
