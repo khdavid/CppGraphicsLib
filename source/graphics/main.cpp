c9b3361b8e8e86566e35fadfe1a60a2f046396f0
David <khdavid@mail.ru>
2018-11-10 10:20:52 +0100

    move tests to test()

--- source/graphics/main.cpp
+++ source/graphics/main.cpp
@@ -5,16 +5,20 @@
 #include "geometryObjects/point2D.h"
 #include "geometryObjects/triangle2D.h"
 
-
-int main(int argc, char** argv)
+void test()
 {
-  
   const Vector2D aa(1, 2);
   auto bb = aa * aa*aa / 7;
   bb[0] = 10;
   auto cc = 2 * bb * 2;
   auto tt = Point2D(1, 2);
   auto ttt = Triangle2D(tt, tt, tt);
+
+}
+
+int main(int argc, char** argv)
+{
+  
   ContextProvider contextProvider;
   MainLoop(contextProvider).run();
   return 0;
