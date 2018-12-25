163b5e555a43d25d578f96365020bb7733c64f94
David <khdavid@mail.ru>
2018-12-25 17:30:31 +0100

    refactor

--- source/graphics/shaders/vertexShader.h
+++ source/graphics/shaders/vertexShader.h
@@ -14,10 +14,12 @@ const std::string cVertexShaderCode = R"(
 #version 130
 
 in vec2 vertexPosition;
+in vec2 colorPosition;
 
 void main()
 {
-  gl_Position.xy = vertexPosition;
+  gl_Position.x = vertexPosition[0];
+  gl_Position.y = vertexPosition[1];
   gl_Position.z = 0.0;
   gl_Position.w = 1.0;
 }
