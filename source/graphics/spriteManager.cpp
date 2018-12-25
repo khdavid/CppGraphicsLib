39ac9f5b63b0ae945676c170498f8817c2c350b8
David <khdavid@mail.ru>
2018-12-26 00:28:01 +0100

    small refactoring

--- source/graphics/spriteManager.cpp
+++ source/graphics/spriteManager.cpp
@@ -7,12 +7,13 @@ SpriteManager::SpriteManager(SDL_Window& window) :
   window_(window),
   sprite_(Triangle2D{ Point2D(0,0), Point2D(-1, -1), Point2D(1, -1) })
 {
-
+  // It is a hack to draw everything during initialization
+  onMouseMovePassive(0, 0);
+  onMouseMovePassive(0, 0);
 }
 
 void SpriteManager::onMouseMovePassive(int x, int y)
 {
-  //glClearDepth(1);
   glClear(GL_COLOR_BUFFER_BIT);
   glClearColor(1.0, 1.0, 1.0, 1.0);
   shaderProgram_.use();
