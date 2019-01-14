afc1171c3f9f313eadef6f89f8695d9e9db80a9c
David <khdavid@mail.ru>
2019-01-14 21:59:12 +0100

    simplify IndexAccessor

--- source/geometry/geometryObjects/indexAccessor.h
+++ source/geometry/geometryObjects/indexAccessor.h
@@ -8,27 +8,13 @@ enum class GeomEntityType
 };
 
 template <class T, size_t N>
-class IndexAccessor
+class IndexAccessor : public std::array<T, N>
 {
 public:
   static constexpr size_t dim = N;
   template <class ...E>
-  IndexAccessor(const E&...args) : arr_  {T(args)...}
+  IndexAccessor(const E&...args) : std::array<T,N>{T(args)...}
   {
-  }
 
-  T& operator[](size_t index)
-  {
-    return arr_[index];
   }
-  const T& operator[](size_t index) const
-  {
-    return arr_[index];
-  }
-  constexpr size_t size() const
-  {
-    return arr_.size();
-  }
-private:
-  std::array<T, N> arr_{}; //{} assures that arr_ is initialized with zeros
  };
