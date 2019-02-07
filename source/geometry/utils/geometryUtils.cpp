6db67cda5a02174ec06f2ea55a3d0628938d2d2f
david.khudaverdyan <d.khudaverdyan@plaxis.com>
2019-02-07 17:02:24 +0100

    some matrix operations

--- source/geometry/utils/geometryUtils.cpp
+++ source/geometry/utils/geometryUtils.cpp
@@ -15,4 +15,28 @@ namespace GeometryUtils
    return result;
 }
 
+ Matrix4D createMatrix4D(const Matrix3D& rotation, const Vector3D& shift)
+ {
+   Matrix4D result = createIdentityMatrix4D();
+   for (size_t i = 0; i < 3; ++i)
+   {
+     for (size_t j = 0; j < 3; ++j)
+     {
+       result[i][j] = rotation[i][j];
+     }
+   }
+
+   for (size_t i = 0; i < 3; ++i)
+   {
+     result[i][3] = shift[i];
+   }
+
+   return result;
+ }
+
+ Matrix3D createRotation(const Vector3D & /*direction*/, double /*angle*/)
+ {
+   return Matrix3D();
+ }
+
 }// end of namespacde GeometryUtils
--- source/geometry/utils/geometryUtils.h
+++ source/geometry/utils/geometryUtils.h
@@ -2,11 +2,15 @@
 #include <array>
 
 #include "geometryObjects/matrix.h"
+#include "geometryObjects/point.h"
 
 
 namespace GeometryUtils
 {
 Matrix4D createIdentityMatrix4D();
+Matrix4D createMatrix4D(const Matrix3D& rotation, const Vector3D& shift = {});
+Matrix3D createRotation(const Vector3D& direction, double angle);
+
 
 template <size_t n, class T>
 std::array<float, n * n> convertToGL(const Matrix<T, n>& matrix)
