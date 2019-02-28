446579a62ade36f8216ae3fca532c5d9735b2893
david.khudaverdyan <d.khudaverdyan@plaxis.com>
2019-02-28 08:22:22 +0100

    remove unnesessary code

--- source/geometry/operations/operations.cpp
+++ source/geometry/operations/operations.cpp
@@ -11,37 +11,3 @@ Vector3D operator-(const Point3D& first, const Point3D& second)
 {
   return Vector3D{ first[0] - second[0], first[1] - second[1], first[2] - second[2]};
 }
-
-int numb()
-{
-  return 0;
-}
-void test2()
-{
-  Vector<double, 4> aaa{ 1.0, 2.0, 3.0,4.0 };
-  Vector<double, 4> bbb{ 1.0, 2, 3, 4 };
-  const int x = 2;
-  Vector<double, 4> abc{ x,x,x,numb() };
-  Vector<double, 4> ddd;
-
-  IndexAccessor<Point2D, 2> tr ( Point2D{1,2},Point2D{1,3} );
-  auto ccc = aaa;
-
-  Vector3D zero;
-  Vector3D aa(1, 2, 3);
-  aa = aa + aa;
-  aa = aa * 2;
-  aa = 2 * aa;
-  aa = -aa;
-  aa = aa / 3;
-
-  Point3D bb;
-  bb + aa;
-
-
-  Point2D cc;
-
-  aa - aa;
-  cc - cc;
-  
-}
