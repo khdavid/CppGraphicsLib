b82f61c6e8c33c86e57b57c3ca693783b75d65bd
David Khudaverdyan <david.khudaverdyan@bentley.com>
2020-10-31 14:31:45 +0100

    Alexandrescu.cpp experiments

--- experiments/Alexandrescu/Alexandrescu/Alexandrescu.cpp
+++ experiments/Alexandrescu/Alexandrescu/Alexandrescu.cpp
@@ -2,28 +2,34 @@
 //
 
 #include <iostream>
-
+#include <string>
 namespace
 {
+size_t gWidgetName = 0;
 
-class Widget
+struct Widget
 {
-public:  
+
   Widget()
   {
-    std::cout << "Widget constructed" << std::endl;
+    std::cout << name << ": widget constructed" << std::endl;
   }
   ~Widget()
   {
-    std::cout << "Widget destructeed" << std::endl;
+    std::cout << name << ": widget destructeed" << std::endl;
   }
-
+  Widget* Clone()
+  {
+    std::cout << name << ": somebody just cloned me" << std::endl;
+    return new(Widget);
+  }
+private:
+  std::string name = std::to_string(gWidgetName++);
 };
 
 template<class T>
-class OpNewCreator
+struct OpNewCreator
 {
-public:
   static T* Create()
   {
     return new(T);
@@ -31,9 +37,8 @@ public:
 };
 
 template<class T>
-class OpMallocCreator
+struct MallocCreator
 {
-public:
   static T* Create()
   {
     auto buf = std::malloc(sizeof(T));
@@ -42,15 +47,38 @@ public:
   }
 };
 
+template<class T>
+struct PrototypeCreator
+{
+  PrototypeCreator(T* obj = nullptr): prototype_(obj)
+  {
+
+  }
+  T* Create()
+  {
+    return prototype_ ? prototype_->Clone() : nullptr;
+  }
+  void SetPrototype(T* pObj)
+  {
+    prototype_ = pObj;
+  }
+  T* GetPrototype()
+  {
+    return prototype_;
+  }
+
+private:
+  T* prototype_;
+};
+
+
 }
 int main()
 {
   OpNewCreator<int>::Create();
-  OpMallocCreator<int>::Create();
-  //auto object = OpMallocCreator<Widget>::Create();
-  auto object = OpNewCreator<Widget>::Create();
-  free(object);
-  delete(object);
-
+  MallocCreator<int>::Create();
+  auto object1 = MallocCreator<Widget>::Create();
+  auto object2 = OpNewCreator<Widget>::Create();
+  auto object3 = PrototypeCreator<Widget>(&Widget()).Create();
 }
 
